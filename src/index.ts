061ac67826cec98224ea8f5fe668dc5150debea9
Abdelaziz Ibrahim <abdelaziz_elshrkawy53@yahoo.com>
2022-09-12 17:20:54 +0200

    error handleing

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ app.get('/', (_req: Request, res: Response): void => {
   res.send(
     "<h1 style='font-size: 3em; text-align: center;'>Welcome to the image processing API</h1> <p style='font-size: 2em; font-weight:500; text-align: center;'> Go to (localhost:" +
       port +
-      '/image?filename=<em>filename here</em>&width=<em>width in munbers here</em>&height=<em>height in numbers here</em>) to resize desired image </p>'
+      '/image?filename=<em>filename here</em>&width=<em>width in numbers here</em>&height=<em>height in numbers here</em>) to resize desired image </p>'
   );
 });
 
